cd582807f0dd2095ac5e5e13b27efc9c179c8dc2
Swordo <swordo95@icloud.com>
2018-11-28 09:12:34 +0530

    Hannoi Tower problem

---
+++ Hanoi tower/hanoi.js
@@ -0,0 +1,15 @@
+function hanoiTower(disk, source, dest, aux) {
+    if (disk == 1) {
+        console.log(`move ${disk} from ${source} to ${dest}`);
+    }
+    else {
+        hanoiTower(disk - 1, source, aux, dest);
+        console.log(`move ${disk} from ${source} to ${dest}`);
+        hanoiTower(disk - 1, aux, dest, source);
+    }
+
+}
+hanoiTower(3, 'A', 'C', 'B');
+
+
+
