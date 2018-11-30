c2d4dff370fceefb4069f4b56e3883fdb735c27f
Swordo <swordo95@icloud.com>
2018-11-30 17:10:00 +0530

    done

--- Hanoi tower/hanoi.js
+++ Hanoi tower/hanoi.js
@@ -1,3 +1,4 @@
+
 function hanoiTower(disk, source, dest, aux) {
     if (disk == 1) {
         console.log(`move ${disk} from ${source} to ${dest}`);
@@ -7,9 +8,10 @@ function hanoiTower(disk, source, dest, aux) {
         console.log(`move ${disk} from ${source} to ${dest}`);
         hanoiTower(disk - 1, aux, dest, source);
     }
-
 }
-hanoiTower(3, 'A', 'C', 'B');
+
+
+hanoiTower(123, 'A', 'C', 'B');
 
 
 
